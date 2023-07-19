6e091d8f9b9a5c44f67714c67344d2b3e95c3938
Lewis Ferguson <lewis.h.ferguson@btinternet.com>
2023-07-19 14:42:15 +0100

    added input to add events to the table

--- controllers/events_controller.py
+++ controllers/events_controller.py
@@ -1,5 +1,5 @@
 from flask import render_template, Blueprint, request
-from models.events_list import events
+from models.events_list import events, add_new_event
 from models.event import Event
 from datetime import date
 
@@ -8,3 +8,13 @@ events_blueprint = Blueprint("events", __name__)
 @events_blueprint.route('/events')
 def index():
     return render_template('index.jinja', title ='Events List', events= events)
+
+
+@events_blueprint.route('/events', methods =["POST"])
+def add_event():
+    event_name = request.form['name']
+    event_room_location = request.form['room_location']
+    event_description = request.form['description']
+    new_event = Event(event_name, event_room_location, event_description)
+    add_new_event(new_event)
+    return render_template('index.jinja', title='Events List', events = events)
--- models/events_list.py
+++ models/events_list.py
@@ -4,3 +4,6 @@ event1 =Event("Music Festival", "Room 3", "Ed-Sheeran Live music")
 event2 = Event("Karting", "Room 2", "Petrol Go-karts")
 
 events = [event1, event2]
+
+def add_new_event(event):
+    events.append(event)
